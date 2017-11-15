4cdb2383e8cda0c2760abb34f3d4e84da0df3bda
Edimilson Work <edimilson.work@gmail.com>
2017-11-15 16:15:07 -0200

    Desabilitando a informação BATIZADO na action Index dos Membros.

--- NovoVivoCaminho/Models/Membros.cs
+++ NovoVivoCaminho/Models/Membros.cs
@@ -11,7 +11,6 @@ namespace NovoVivoCaminho.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     
     public partial class Membros
     {
@@ -27,27 +26,17 @@ namespace NovoVivoCaminho.Models
         public string Nome { get; set; }
         public string Tipo { get; set; }
         public string Endereco { get; set; }
-
         public string Numero { get; set; }
-
         public string Complemento { get; set; }
         public string Bairro { get; set; }
         public string Cidade { get; set; }
         public string UF { get; set; }
         public string CEP { get; set; }
-
         public string DDD { get; set; }
-
         public string Fone { get; set; }
         public string EstadoCivil { get; set; }
-        public Nullable<bool> Batizado { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public bool Batizado { get; set; }
         public Nullable<System.DateTime> DataDeNascimento { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> MembroDesde { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- NovoVivoCaminho/Models/NVCModel.Designer.cs
+++ NovoVivoCaminho/Models/NVCModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Edimilson Jr\Source\repos\NovoVivoCaminho\NovoVivoCaminho\Models\NVCModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Edimilson Jr\Documents\GitHub\NovoVivoCaminho\NovoVivoCaminho\Models\NVCModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
