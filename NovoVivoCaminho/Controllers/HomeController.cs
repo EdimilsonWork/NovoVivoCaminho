adf51e94ee24dff39de2996b85b4eb9dabe03d0e
Edimilson Work <edimilson.work@gmail.com>
2018-01-07 15:56:33 -0200

    Inicio do controle de acesso. v.7

--- NovoVivoCaminho/Controllers/HomeController.cs
+++ NovoVivoCaminho/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NovoVivoCaminho.Models;
 
 namespace NovoVivoCaminho.Controllers
 {
@@ -10,7 +11,39 @@ namespace NovoVivoCaminho.Controllers
     {
         public ActionResult Index()
         {
+            if (Session["usuarioLogadoID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Login()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuarios usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                using(NVCEntities nvc = new NVCEntities())
+                {
+                    var u = nvc.Usuarios.Where(x => x.Login.Equals(usuario.Login) && x.Senha.Equals(usuario.Senha)).FirstOrDefault();
+                    if (u != null)
+                    {
+                        Session["usuarioLogadoID"] = u.ID.ToString();
+                        Session["nomeUsuarioLogado"] = u.Nome.ToString();
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            return View(usuario);
         }
 
         public ActionResult About()
